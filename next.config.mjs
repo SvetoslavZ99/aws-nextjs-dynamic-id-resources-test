1402822c943b740b286640d4493c216e13b7e47a
I742856 <svetoslav.zaykov@sap.com>
2024-04-04 16:39:42 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/SomeBasePath",
+  generateBuildId: () => {
+    return "020d8ebcd5SomeRandomHash4c23ceeda7cd9a30d82";
+  },
 };
 
 export default nextConfig;
